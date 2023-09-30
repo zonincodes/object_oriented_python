4368ace93396d5cdb6f53b1637c944ae7ea49d4e
Zonin <business.kariukimartin@gmail.com>
2023-09-30 06:36:57 +0300

    classes and methods

--- dog.py
+++ dog.py
@@ -25,16 +25,40 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name"""
         long_name = str(self.year) + " " + self.make + " " + self.model
         return long_name.title()
 
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def read_odometer(self):
+        """Print a statement showing the car millage"""
+        print(f"This car has {str(self.odometer_reading)} miles on it.")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
 
 my_new_car = Car('audi', 'a4', 2016)
 my_other_car = Car("Toyota", "Camry", 1964)
-
+my_old_car = Car("Subaru", "Outback", 2020)
+my_old_car.update_odometer(20000)
+my_old_car.increment_odometer(100)
 if __name__ == "__main__":
     print(my_dog.name.title())
     print(my_other_car.get_descriptive_name())
+    help(Car)
+    my_old_car.read_odometer()
